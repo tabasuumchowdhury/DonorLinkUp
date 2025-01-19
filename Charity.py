2d918ff1eb986b222c6bb77561fbad0eceee73b4
adja-camara <adja.camara@mail.mcgill.ca>
2025-01-19 00:43:15 -0500

    Update Charity.py

--- Charity.py
+++ Charity.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 class Charity:
     bn = ""
@@ -12,10 +13,8 @@ class Charity:
     postalCode = ""
     country = ""
 
-    def __init__(self, bn, cat,
-                 legalName, accName, addressLine1,
-                 addressLine2, city, province,
-                 postalCode, country):
+    def __init__(self, bn, cat, legalName, accName, addressLine1, 
+                 addressLine2, city, province, postalCode, country):
         self.bn = bn
         self.cat = cat
         self.legalName = legalName
@@ -42,32 +41,45 @@ class Charity:
             data.get('country', "")
         )
 
+    @classmethod
     def from_csv(cls, file_path):
         charities = []
         df = pd.read_csv(file_path, encoding='utf-8', on_bad_lines="skip", 
-                keep_default_na=False, na_values=["", "NA", "N/A"])
+                         keep_default_na=False, na_values=["", "NA", "N/A"])
         for _, row in df.iterrows():
             charity_instance = cls(
-                bn=row['BN'],
-                cat=row['Category'],
-                legalName=row['Legal Name'],
-                accName=row['Account Name'],
-                addressLine1=row['Address Line 1'],
-                addressLine2=row['Address Line 2'],
-                city=row['City'],
-                province=row['Province'],
-                postalCode=row['Postal Code'],
-                country=row['Country']
+                bn=str(row['BN']),
+                cat=str(row['Category']),
+                legalName=str(row['Legal Name']),
+                accName=str(row['Account Name']),
+                addressLine1=str(row['Address Line 1']),
+                addressLine2=str(row['Address Line 2']),
+                city=str(row['City']),
+                province=str(row['Province']),
+                postalCode=str(row['Postal Code']),
+                country=str(row['Country'])
             )
             charities.append(charity_instance)
         return charities
+    
+    def get_city(self):
+        return self.city
+    
+    def get_category(self):
+        return self.cat
+    
+    def to_json(self):
+        obj = {"bn",self.bn, "cat", self.cat, "city", self.city}
+        return json.dumps(obj)
+    
+    def __str__(self):
+        return self.to_json()
 
-    def charity_search(category = "", city = ""):
-        charities = from_csv(Charity, "charity.csv")
+    @classmethod
+    def charity_search(cls, category="", city=""):
+        charities = cls.from_csv("charity.csv")
         charity_chosen = []
         for charity in charities:
-            if ((category == "" and charity.city == city) or 
-                (charity.cat == category and charity.city == "") or
-                (charity.cat == category and charity.city == city)) :
+            if (category.lower() in charity.get_category().lower()) and (city.lower() in charity.get_city().lower()):
                 charity_chosen.append(charity)
-    return charity_chosen
+        return charity_chosen
