bb19d6b32333df34d7c74f71fdd56d59e3b23eda
jessicasunyay <jessicasunyay@gmail.com>
2025-01-18 22:17:27 -0500

    Final working submit errors!

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.querySelector('interest-button').addEventListener('click', function() {
     this.classList.toggle("clicked");
   });
+
+  async function submitSignup() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const hometown = document.getElementById("hometown").value;
+    const message = document.getElementById("emptyField");
+
+    if (!name || !email || !hometown) {
+        message.style.display = "block";
+    }
+    else{
+        window.location.href="interests.html";
+    }
+}
