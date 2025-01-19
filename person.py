2fe6e1c7b75266e0603e700a54fd7a9228b8eac6
adja-camara <adja.camara@mail.mcgill.ca>
2025-01-19 00:44:05 -0500

    Update person.py

--- person.py
+++ person.py
@@ -1,31 +1,26 @@
 from Charity import Charity
 
-class Person : 
-    cur = 0
-    name = ""
-    email = ""
-    interest = ""
-    hometown = ""
-    matches = list() # keeps track of matches
+class Person:
     def __init__(self, name, email, interest, hometown):
         self.name = name
         self.email = email
         self.interest = interest
         self.hometown = hometown
+        self.matches = []
 
+    def get_interest(self):
+        return self.interest
 
-def get_interest(self) :
-    return self.interest
+    def get_city(self):
+        return self.hometown
 
-def get_city(self) :
-    return self.hometown
+    def return_matches(self) -> list:
+        matches = []
+        precise = Charity.charity_search(self.interest, self.hometown)
+        interest_list = Charity.charity_search(self.interest, "")
+        city_list = Charity.charity_search("", self.hometown)
+        matches.extend(precise)
+        matches.extend(interest_list)
+        matches.extend(city_list)
+        return matches
 
-def return_matches(person: Person) -> list : # returns all of the matches, no repeats
-    matches = []
-    precise = Charity.charity_search(person.interest, person.hometown)
-    interest_list = Charity.charity_search(person.interest, "")
-    city_list = Charity.charity_search("", person.hometown)
-    matches = matches.extend(precise)
-    matches = matches.extend(interest_list)
-    matches = matches.extend(city_list)
-    return matches
