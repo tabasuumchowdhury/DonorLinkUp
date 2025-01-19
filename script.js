ae2a01c2db26f33ab60ee7510d14c3a1c39a5435
jessicasunyay <jessicasunyay@gmail.com>
2025-01-19 09:51:48 -0500

    corrected errors i think

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function changeButtonColor()
 {
     
 }
+document.querySelector('interest-button').addEventListener('click', function() {
+    this.classList.toggle("clicked");
+  });
 
-  async function submitSignup() {
+async function submitSignup() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const hometown = document.getElementById("hometown").value;
     const messageEmpty = document.getElementById("emptyField");
     const messageEmail = document.getElementById("invalidEmail");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    messageEmpty.style.display = "none";
-    messageEmail.style.display = "none";
 
     if (!name || !email || !hometown) {
         messageEmpty.style.display = "block";
